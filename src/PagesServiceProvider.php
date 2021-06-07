28992265a39868b03e9e88b71518e3a3e19edecf
GIS <dev@gis4biz.ru>
2021-06-07 13:31:27 +0300

    Initial commit

---
+++ src/PagesServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Notabenedev\SitePages;
+
+use Illuminate\Support\ServiceProvider;
+
+class PagesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
