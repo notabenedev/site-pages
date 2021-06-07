f488acb4e818b33cb0849a474a2ba424773d8587
GIS <dev@gis4biz.ru>
2021-06-07 14:21:39 +0300

    Take on migration

--- src/PagesServiceProvider.php
+++ src/PagesServiceProvider.php
@@ -6,6 +6,23 @@ use Illuminate\Support\ServiceProvider;
 
 class PagesServiceProvider extends ServiceProvider
 {
+    
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //Публикация конфигурации
+        $this->publishes([__DIR__.'/config/pages.php' => config_path('pages.php'),
+                ], 'config');
+
+        //Подключение миграций
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+    }
+
     /**
      * Register services.
      *
@@ -15,17 +32,6 @@ class PagesServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(
             __DIR__.'/config/pages.php', 'pages');
-        
-    }
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->publishes([__DIR__.'/config/pages.php' => config_path('pages.php'),
-                ], 'config');
     }
 }
