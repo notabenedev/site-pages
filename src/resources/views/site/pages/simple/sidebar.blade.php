0471afc38de27ccf02ccd916473a597ef8e39fd1
GIS <dev@gis4biz.ru>
2022-03-22 14:53:58 +0300

    change css, sticky sidebar

--- src/resources/views/site/pages/simple/sidebar.blade.php
+++ src/resources/views/site/pages/simple/sidebar.blade.php
@@ -1,5 +1,6 @@
-@if(!empty($img))
-    @pic([
+<div class="sticky-top">
+    @if(!empty($img))
+        @pic([
         "image" => $img,
         "template" => "pages-grid-sm-12",
         "grid" => [
@@ -9,11 +10,13 @@
         ],
         "imgClass" => "img-fluid rounded mb-4",
         ])
-@endisset
-<div class="page-simple__form">
-    @if(config("site-pages.siteSimplePageFormHeader", false))
-        <h3 class="h3 page-simple__form_header">{{ config("site-pages.siteSimplePageFormHeader") }}</h3>
-    @endif
-    @include('site-pages::site.pages.includes.form-question',["title" => $title])
+    @endisset
+    <div class="page-simple__form">
+        @if(config("site-pages.siteSimplePageFormHeader", false))
+            <h3 class="h3 page-simple__form_header">{{ config("site-pages.siteSimplePageFormHeader") }}</h3>
+        @endif
+        @include('site-pages::site.pages.includes.form-question',["title" => $title])
+    </div>
 </div>
+
 
